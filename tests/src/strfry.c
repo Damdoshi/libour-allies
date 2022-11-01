9fdd8e931a9a49571361e2d27784675cb544f6ff
Beskoo <104134078+Beskoo@users.noreply.github.com>
2022-11-01 19:50:10 +0100

    finish strfry

---
+++ tests/src/strfry.c
@@ -0,0 +1,9 @@
+#include "ourstring.h"
+#include <stdlib.h>
+
+
+int main(void) {
+  assert(our_strfry("Hello World") == "lllr doWeoH");
+  assert(our_strfry("Hello World") == "lllr doWeoH");
+  return (EXIT_SUCCESS);
+}
