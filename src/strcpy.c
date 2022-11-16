f16351428a216b4e8f00181d4deb6852790addf0
shause08 <91192225+shause08@users.noreply.github.com>
2022-11-16 10:51:03 +0100

    Add strcpy function

---
+++ src/strcpy.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "ourstring.h"
+
+char *our_strcpy(const char *s)
+{
+    if(s == NULL){
+        return NULL;
+    }
+    char *d = malloc(sizeof(s));
+    for(int i = 0; i < our_strlen(s); i++) {
+        d[i] = s[i];
+    }
+    return d;
+}
