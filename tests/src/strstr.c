e9c82bdf8466b1bc59263fa681292a6c7e1a038f
WhiteColors_ <49518591+WhiteColors@users.noreply.github.com>
2022-11-16 11:49:26 +0100

    Update strstr.c

--- tests/src/strstr.c
+++ tests/src/strstr.c
@@ -1,13 +1,13 @@
 #include	<stdlib.h>
+#include	<string.h>
 #include	"ourstring.h"
 
 int	main(void){
-  assert(our_strstr("Hello World", "World") == "World");
-  assert(our_strstr("Hello World", "Hello") == "Hello World");
-  assert(our_strstr("Hello World", "Hello World") == "Hello World");
-  assert(our_strstr("Hello World", "Hello World!") == NULL);
-  assert(our_strstr("Hello World", "salut") == NULL);
-  assert(our_strstr("Hello World", "Hello World Hello World") == NULL);
+  assert(strcmp(our_strstr("Hello World", "World"), "World") == 0);
+  assert(strcmp(our_strstr("Hello World", "Hello"), "Hello World") == 0);
+  assert(strcmp(our_strstr("Hello World", "Hello World"), "Hello World") == 0);
+  assert(strcmp(our_strstr("Hello World", "Hello World!"), NULL) == 0);
+  assert(strcmp(our_strstr("Hello World", "salut"), NULL) == 0);
+  assert(strcmp(our_strstr("Hello World", "Hello World Hello World"), NULL) == 0);
   return (EXIT_SUCCESS);
 }
-
