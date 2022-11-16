4bf65e6790395abc85650c1e877f14505d7d534f
hugocnvs <hugo@canavese.biz>
2022-11-16 16:11:45 +0100

    Add our_54 memchr.c

--- include/ourstring.h
+++ include/ourstring.h
@@ -28,5 +28,6 @@ char *our_strcat(char *dest, const char *src);
 char *our_strcpy(const char *s);
 char *our_strfry(char *str);
 size_t our_strcspn(const char *string, const char *rejected);
+void *our_memchr(const void *block, int search, size_t size);
 #endif /*	OURSTRING_H		*/
 
