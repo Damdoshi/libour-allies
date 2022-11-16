c50b72d0241eef36d799806be4b1dc30987a62c0
shause08 <91192225+shause08@users.noreply.github.com>
2022-11-16 16:29:52 +0100

    real strcpy test

--- tests/src/strcpy.c
+++ tests/src/strcpy.c
@@ -1,34 +1,14 @@
 #include <stdlib.h>
-#include <assert.h>
 #include <stdio.h>
+#include <string.h>
+#include <assert.h>
 #include "ourstring.h"
 
 int	main(void)
 {
-  const char *source = "abc";
-  char *dest = (our_strcpy(source));
-  int result = 0;
-  if(*dest++ == *source++){
-    result = 1;
-  }
-  assert(result == 1);
-  result = 0;
-
-  const char *source2 = "abc\0def";
-  char *dest2 = (our_strcpy(source2));
-  if(*dest2++ == *source2++){
-    result = 1;
-  }
-  assert(result == 1);
-  result = 0;
-
-  const char *source3 = "1";
-  char *dest3 = (our_strcpy(source3));
-  if(*dest3++ == *source3++){
-    result = 1;
-  }
-  assert(result == 1);
-  result = 0;
-
+  const char src[4] = "ABC";
+  char dest[4];
+  our_strcpy(dest,src);
+  assert(strcmp(dest,"ABC")==0);
   return (EXIT_SUCCESS);
 }
