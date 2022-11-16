ff60cba5b18650223eed581d61db36d0eaae4194
hadrien-samrek lacombe <hadriensamrek@gmail.com>
2022-11-16 15:09:43 +0100

    explode

--- src/explode.c
+++ src/explode.c
@@ -3,32 +3,24 @@
 #include <string.h>
 #include <stdio.h>
 
-char *strdup(const char *src)
-{
-    char *tmp = malloc(strlen(src) + 1);
-    if(tmp)
-        strcpy(tmp, src);
-    return tmp;
-}
-
 int our_explode(const char *src, const char *tokens, char ***list, size_t *len)
 {   
     efassert("src is NULL", src, return(-1));
     efassert("tokens is NULL", tokens, return(-1));
     efassert("list is NULL", list, return(-1));
     efassert("len is NULL", len, return(-1));
-    efassert("tokens is empty", strlen(tokens), return(-1));
-    efassert("src is empty", strlen(src), return(-1));
+    efassert("tokens is empty", our_strlen(tokens), return(-1));
+    efassert("src is empty", our_strlen(src), return(-1));
 
     char *str, *copy, **_list = NULL, **tmp;
     *list = NULL;
     *len  = 0;
 
-    copy = strdup(src);
+    copy = our_strdup(src);
     if(copy == NULL)
         return -1;
 
-    str = strtok(copy, tokens);
+    str = our_strtok(copy, tokens);
     if(str == NULL) {
         return -1;
     }
@@ -38,14 +30,14 @@ int our_explode(const char *src, const char *tokens, char ***list, size_t *len)
         return -1;
     }
 
-    _list[*len] = strdup(str);
+    _list[*len] = our_strdup(str);
     if(_list[*len] == NULL) {
         return -1;
     }
     (*len)++;
 
 
-    while((str = strtok(NULL, tokens)))
+    while((str = our_strtok(NULL, tokens)))
     {
         tmp = realloc(_list, (sizeof *_list) * (*len + 1));
         if(tmp == NULL) {
@@ -53,7 +45,7 @@ int our_explode(const char *src, const char *tokens, char ***list, size_t *len)
         }
         _list = tmp;
 
-        _list[*len] = strdup(str);
+        _list[*len] = our_strdup(str);
         if(_list[*len] == NULL) {
             return -1;
         }
